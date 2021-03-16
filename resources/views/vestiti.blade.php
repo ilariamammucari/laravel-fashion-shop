cdfb2baa97dcf6fa0bbda3ef3aec480db19784b6
Ilaria <ilariamammucari@hotmail.it>
2021-03-16 15:20:07 +0100

    modifiche bootstrap

--- resources/views/vestiti.blade.php
+++ resources/views/vestiti.blade.php
@@ -3,14 +3,24 @@
 
 @section('content')
     <main>
-        <ul>
-            @foreach ($dresses as $dress)
-                <li>
-                    <p>{{ $dress->tipo }}</p>
-                    <p>- {{ $dress->tessuto }}</p>
-                    <p>- {{ $dress->taglia }}</p>
-                </li>
-            @endforeach
-        </ul>
+        <div class="container">
+            <div class="d-flex">
+                @foreach ($dresses as $dress)
+
+                    <div class="p-5">
+                        {{ $dress->tipo }}:
+                        <ul>
+                            <li>
+                                {{ $dress->tessuto }}
+                            </li>
+                            <li>
+                                {{ $dress->taglia }}
+                            </li>
+                        </ul>
+                    </div>
+
+                @endforeach
+            </div>
+        </div>
     </main>
 @endsection
